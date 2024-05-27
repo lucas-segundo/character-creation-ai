339cf8d88bc57eceb0155f045c3a18e11663eab3
Lucas Segundo <lucas.fernandes.app@gmail.com>
2024-05-27 16:02:46 -0300

    refactor: improves Header component styling

--- src/presentation/components/organisms/Header/index.tsx
+++ src/presentation/components/organisms/Header/index.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <div className="flex justify-between items-center w-full p-4 bg-black">
+    <div className="flex items-center p-4 bg-black">
       <h1 className="font-bold text-white">Character Creation</h1>
     </div>
   )
