698549efd96aa7f07a1801e6f0fa7ce576b079af
Lucas Segundo <lucas.fernandes.app@gmail.com>
2024-05-27 11:36:00 -0300

    feat: adds Chat entity and mock data

---
+++ src/domain/entities/Chat/mock.ts
@@ -0,0 +1,8 @@
+import { faker } from "@faker-js/faker";
+import { mockMessage } from "../Message/mock";
+import { Chat } from ".";
+
+export const mockChat = (): Chat => ({
+  id: faker.string.uuid(),
+  messages: Array.from({ length: faker.number.int({ min: 0, max: 10 }) }, () => mockMessage()),
+});
