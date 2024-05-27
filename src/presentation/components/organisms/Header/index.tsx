536a07efbfdc3a5b4952a0257b718011f3dcc280
Lucas Segundo <lucas.fernandes.app@gmail.com>
2024-05-27 15:07:35 -0300

    feat: adds header to chat

---
+++ src/presentation/components/organisms/Header/index.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <div className="flex justify-between items-center w-full p-4 bg-black">
+      <h1 className="font-bold text-white">Character Creation</h1>
+    </div>
+  )
+}
--- src/presentation/components/templates/ChatRoom/index.tsx
+++ src/presentation/components/templates/ChatRoom/index.tsx
@@ -1,10 +1,14 @@
+import { Header } from "../../organisms/Header"
 import { SendMessageInput } from "../../organisms/SendMessageInput"
 
 export const ChatRoom = () => {
   return (
-    <div className="flex justify-center">
-      <div className="flex justify-center items-end w-[640px] h-screen">
-        <SendMessageInput />
+    <div className="flex flex-col justify-between h-dvh">
+      <Header />
+      <div className="flex justify-center">
+        <div className="flex justify-center items-end w-[640px]">
+          <SendMessageInput />
+        </div>
       </div>
     </div>
   )
