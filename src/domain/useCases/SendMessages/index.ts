ce597411fe9f1a34739db54afb1304b34587e29d
Lucas Segundo <lucas.fernandes.app@gmail.com>
2024-05-28 11:56:13 -0300

    feat: returns array of messages for send messages

--- src/domain/useCases/SendMessages/index.ts
+++ src/domain/useCases/SendMessages/index.ts
@@ -3,6 +3,6 @@ import { Message } from "@/domain/entities/Message";
 export namespace SendMessages {
   export type Messages = Pick<Message, 'text' | 'chatID' | 'sender'>[]
   export interface UseCase {
-    send: (messages: Messages) => Promise<Message>;
+    send: (messages: Messages) => Promise<Message[]>;
   }
 }
