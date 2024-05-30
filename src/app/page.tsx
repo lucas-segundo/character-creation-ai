478720ce874d14e90e8537697dfdd96394ff4753
Lucas Segundo <lucas.fernandes.app@gmail.com>
2024-05-30 14:43:21 -0300

    feat: adds error notifier to ChatRoom component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 'use client'
+import { makeErrorNotifier } from '@/factories/presentation/makeErrorNotifier'
 import { makeSendMessagesUseCase } from '@/factories/useCases/SendMessages'
 import { ChatRoom } from '@/presentation/components/templates/ChatRoom'
 
 export default function Home() {
-  return <ChatRoom sendMessageUseCase={makeSendMessagesUseCase()} />
+  return (
+    <ChatRoom
+      sendMessageUseCase={makeSendMessagesUseCase()}
+      errorNotifier={makeErrorNotifier()}
+    />
+  )
 }
