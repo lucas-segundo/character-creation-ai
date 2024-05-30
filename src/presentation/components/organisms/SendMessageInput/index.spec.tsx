a5c2b4f3e9b089e1c8ee4fca58726e2eff628d66
Lucas Segundo <lucas.fernandes.app@gmail.com>
2024-05-30 14:08:43 -0300

    fix: test should call onSend with right params

--- src/presentation/components/organisms/SendMessageInput/index.spec.tsx
+++ src/presentation/components/organisms/SendMessageInput/index.spec.tsx
@@ -1,22 +1,20 @@
-import { act, render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { describe, expect, it, vi } from 'vitest';
 import { SendMessageInput } from '.';
 import userEvent from '@testing-library/user-event';
 import { faker } from '@faker-js/faker';
 
 describe('SendMessageInput', () => {
-  it('should call onSend with right params', () => {
+  it('should call onSend with right params', async () => {
     const mockedOnSend = vi.fn();
     render(<SendMessageInput onSend={mockedOnSend} />);
 
     const user = userEvent.setup();
     const text = faker.lorem.sentence();
 
-    act(() => {
-      user.type(screen.getByTestId('message-input'), text);
-      user.click(screen.getByTestId('send-button'));
-    });
+    await user.type(screen.getByTestId('message-input'), text);
+    await user.click(screen.getByTestId('send-button'));
 
-    waitFor(() => expect(mockedOnSend).toBeCalledWith({ text }));
+    expect(mockedOnSend).toBeCalledWith({ text });
   });
 });
