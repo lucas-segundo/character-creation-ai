53b2a0af46668532ac2a7fa1678087232d64099f
Lucas Segundo <lucas.fernandes.app@gmail.com>
2024-05-30 08:41:36 -0300

    feat: adds SendMessageInput unit test

---
+++ src/presentation/components/organisms/SendMessageInput/index.spec.tsx
@@ -0,0 +1,22 @@
+import { act, render, screen, waitFor } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
+import { SendMessageInput } from ".";
+import userEvent from "@testing-library/user-event";
+import { faker } from "@faker-js/faker";
+
+describe("SendMessageInput", () => {
+  it("should call onSend with right params", () => {
+    const mockedOnSend = vi.fn()
+    render(<SendMessageInput onSend={mockedOnSend} />)
+
+    const user = userEvent.setup()
+    const text = faker.lorem.sentence()
+
+    act(() => {
+      user.type(screen.getByTestId('message-input'), text)
+      user.click(screen.getByTestId('send-button'))
+    })
+
+    waitFor(() => expect(mockedOnSend).toBeCalledWith({ text }))
+  });
+})
--- src/presentation/components/organisms/SendMessageInput/index.tsx
+++ src/presentation/components/organisms/SendMessageInput/index.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { MdSend } from "react-icons/md";
 
 interface OnSendParams {
-  text: string | null
+  text: string
 }
 
 interface Props {
@@ -17,11 +17,14 @@ export const SendMessageInput = ({ onSend }: Props) => {
         <div className="flex justify-between items-center border border-gray-800 p-4 rounded">
           <input 
             className="mr-2 text-gray-400 bg-black w-full focus:outline-none" 
-            name="full_name" 
+            name="message" 
+            data-testid="message-input"
             placeholder="Send a message"
+            min={3}
+            value={text}
             onChange={(event) => setText(event.currentTarget.value)}
           />
-          <MdSend onClick={() => onSend({ text })} size={25} color="white"/> 
+          <MdSend data-testid="send-button" onClick={() => onSend({ text })} size={25} color="white"/> 
         </div>
       </div>
     </div>
