bc5a1a152b1a273586e214a8e27d49f33be57cbe
Lucas Segundo <lucas.fernandes.app@gmail.com>
2024-06-03 16:06:21 -0300

    feat: changes mockMessage function to accept optional sender parameter

--- src/domain/entities/Message/mock.ts
+++ src/domain/entities/Message/mock.ts
@@ -1,12 +1,18 @@
 import { faker } from '@faker-js/faker'
 import { Message } from '.'
 
-export const mockMessage = (): Message => ({
+interface Params {
+  sender?: Message['sender']
+}
+
+export const mockMessage = (params?: Params): Message => ({
   id: faker.string.uuid(),
   text: faker.lorem.sentences(),
   chatID: faker.string.uuid(),
-  sender: faker.number.int({
-    min: 0,
-    max: 2,
-  }) as Message['sender'],
+  sender:
+    params?.sender ||
+    (faker.number.int({
+      min: 0,
+      max: 2,
+    }) as Message['sender']),
 })
