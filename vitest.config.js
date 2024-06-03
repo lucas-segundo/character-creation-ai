643358982bbc82b8a5bce44565eef2473013a69a
Lucas Segundo <lucas.fernandes.app@gmail.com>
2024-06-03 14:09:50 -0300

    chore: configs @testing-library/jest-dom

--- vitest.config.js
+++ vitest.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
- 
+
 export default defineConfig({
   plugins: [react()],
   test: {
+    globals: true,
     environment: 'jsdom',
+    setupFiles: './vitest.setup.js',
   },
   resolve: {
     alias: {
-      '@': '/src'
-    }
-  }
+      '@': '/src',
+    },
+  },
 })
