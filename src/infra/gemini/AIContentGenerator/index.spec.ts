ac72a2ba51f2bc154fda82ff4483b24b07cbaf65
Lucas Segundo <lucas.fernandes.app@gmail.com>
2024-06-19 09:28:24 -0300

    refactor: improves GeminiAIContentGenerator to improve code readability and maintainability

--- src/infra/gemini/AIContentGenerator/index.spec.ts
+++ src/infra/gemini/AIContentGenerator/index.spec.ts
@@ -5,22 +5,29 @@ import { faker } from '@faker-js/faker'
 import { mockChatSession } from '../mock'
 import { ChatSession, GenerateContentResult } from '@google/generative-ai'
 
-describe('GeminiAIContentGenerator', () => {
+const makeSUT = () => {
   const chatSession = mockChatSession()
   const sut = new GeminiAIContentGenerator(
     chatSession as unknown as ChatSession,
   )
 
+  return { sut, chatSession }
+}
+
+describe('GeminiAIContentGenerator', () => {
   it('should call sendMessage on ChatSession with correct parameters', async () => {
-    const params: AIContentGeneratorParams = {
-      texts: [faker.lorem.paragraph()],
-    }
+    const { sut, chatSession } = makeSUT()
+
     const result = {
       response: {
         text: () => faker.lorem.paragraph(),
       },
     } as unknown as GenerateContentResult
     chatSession.sendMessage.mockResolvedValue(result)
+
+    const params: AIContentGeneratorParams = {
+      texts: [faker.lorem.paragraph()],
+    }
     await sut.generate(params)
 
     expect(chatSession.sendMessage).toHaveBeenCalledWith(params.texts)
