c7035086a6601b4bd2b0d5ab10892fbd8a1b2844
Lucas Segundo <lucas.fernandes.app@gmail.com>
2024-06-20 08:38:04 -0300

    refactor: indentations on imports in makeCreateMessages.ts

--- src/factories/useCases/makeCreateMessages.ts
+++ src/factories/useCases/makeCreateMessages.ts
@@ -1,7 +1,5 @@
 import { CreateMessagesImpl } from '@/data/useCases/CreateMessages'
-import {
-  CreateMessagesUseCase,
-} from '@/domain/useCases/CreateMessages'
+import { CreateMessagesUseCase } from '@/domain/useCases/CreateMessages'
 import { LocalStorageMessagesCreater } from '@/infra/localStorage/MessagesCreater'
 
 export const makeCreateMessagesUseCase = (): CreateMessagesUseCase => {
